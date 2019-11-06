df303c9820c46be944a20af68d68d91893ab448d
analBrotherhood <57309597+analBrotherhood@users.noreply.github.com>
2019-11-06 12:37:42 +0300

    Update choosing.py

--- choosing.py
+++ choosing.py
@@ -1,4 +1,4 @@
-import random
+from random import choice as ch
 
 print('''\nМонетка 2.0 | usu4lc0d3r (c)
 Вводи варианты, между которыми выбрать, один за другим
@@ -16,7 +16,7 @@ if len(choices) == 0:
     print('Ничего не введено. Всего доброго! :D')
     exit()
 else: 
-    choice = random.choice(choices)
+    choice = ch(choices)
     try:
         print('Мой выбор: ', choice)
     except IndexError:
